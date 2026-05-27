76e3f3b3f8c4df2ae278c4f96f172e6551918b8d
Otaviocuriel <otavio.curiel345@gmail.com>
2026-05-27 08:35:28 -0300

    Criando a estrutura do React

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+const app = () => {
+    return (
+        <div>
+            <h1> Meu projeto react</h1>
+        </div>
+    );
+};
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {createRoot} from "react-dom";
+import app from "./app"; 
+
+
+const root = createRoot(document.getElementById("root"));
+root.render(<app />);
