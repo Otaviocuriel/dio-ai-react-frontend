cba7e766783926ff063b201ff027bb02b2b491e0
Otaviocuriel <otavio.curiel345@gmail.com>
2026-05-27 14:59:57 -0300

    Finalizando as configuração do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require ("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports ={
 
@@ -6,6 +7,36 @@ module.exports ={
     output: {
         path: path.resolve(__dirname, "dist"),
 
-        filename:"bundle.js"
-    }
-}
+        filename:"bundle.js",
+
+    },
+
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+
+                exclude: /node_modules/,
+
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ],
+    resolve:{
+        extensions:[".js",".jsx"]
+    },
+    devServer:{
+        static:{
+            directory: path.join(__dirname, "dist")
+        },
+        port: 3000,
+    },
+};
