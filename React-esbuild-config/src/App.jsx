3da377dab83d8f77018b708df5f4b86a33e6805f
Otaviocuriel <otavio.curiel345@gmail.com>
2026-06-01 08:15:28 -0300

    Configurando servidor de desenvolvimento

--- React-esbuild-config/src/App.jsx
+++ React-esbuild-config/src/App.jsx
@@ -5,7 +5,10 @@ const App = () => {
 
         <div>
             <h1>
-                Meu Projeto com esbuild!
+                Meu Projeto com esbuild!!
+            <div>
+              <h5> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, sint velit fugit maxime voluptatem eaque nesciunt, ratione quia suscipit itaque quod a ex neque deleniti voluptates ipsa dicta nulla qui.</h5>
+            </div>    
             </h1>
     
         </div>    
