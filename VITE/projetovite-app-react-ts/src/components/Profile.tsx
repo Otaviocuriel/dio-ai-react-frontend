965c9d142da1515ca53f5bd9fef3ce86b431595b
Otaviocuriel <otavio.curiel345@gmail.com>
2026-06-11 15:33:37 -0300

    Centralizando

--- VITE/projetovite-app-react-ts/src/components/Profile.tsx
+++ VITE/projetovite-app-react-ts/src/components/Profile.tsx
@@ -4,10 +4,11 @@ import { Skills } from './Skills'
 const Profile: React.FC = () => {
     return (
         <Fragment>
-            <img src="https://assets.dio.me/odR66-3cLVSUwAcVAwmXdIaemT5GLsRhssECa5Bs3yA/f:webp/h:96/q:80/w:96/L3VzZXJzL3N0dWRlbnQvYWI1MmE5NTItM2IzNC00NWM3LWE4ZjYtZDY3ZjFlNmE5NjA3LmpwZw"
-         alt="Otavio"
-         width="300px"
-         />
+            <img 
+      src="https://assets.dio.me/odR66-3cLVSUwAcVAwmXdIaemT5GLsRhssECa5Bs3yA/f:webp/h:96/q:80/w:96/L3VzZXJzL3N0dWRlbnQvYWI1MmE5NTItM2IzNC00NWM3LWE4ZjYtZDY3ZjFlNmE5NjA3LmpwZw"
+      alt="Otavio"
+      style={{ width: '300px', display: 'block', margin: '0 auto' }} 
+    />
         
 
        <h3>
