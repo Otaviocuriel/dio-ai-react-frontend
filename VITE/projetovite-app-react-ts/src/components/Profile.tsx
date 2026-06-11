d374cddb50cc75344d3d50ff8911f2b0a8c6c3e4
Otaviocuriel <otavio.curiel345@gmail.com>
2026-06-11 10:54:55 -0300

    Finalizando a estrutura

--- VITE/projetovite-app-react-ts/src/components/Profile.tsx
+++ VITE/projetovite-app-react-ts/src/components/Profile.tsx
@@ -3,13 +3,18 @@ const Profile: React.FC = () => {
     return (
         <div>
             <img src="https://assets.dio.me/odR66-3cLVSUwAcVAwmXdIaemT5GLsRhssECa5Bs3yA/f:webp/h:96/q:80/w:96/L3VzZXJzL3N0dWRlbnQvYWI1MmE5NTItM2IzNC00NWM3LWE4ZjYtZDY3ZjFlNmE5NjA3LmpwZw"
-         alt="Otavio" />
+         alt="Otavio"
+         width="300px"
+         />
+        
 
        <h3>
         Otavio Curiel
        </h3>
        <p>
-        TI
+       <b>
+         TI
+        </b>
        </p> 
        </div>
          
