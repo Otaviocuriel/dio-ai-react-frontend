62fcf3ff0c2d5cdae1a24f6b9439086898300980
Otaviocuriel <otavio.curiel345@gmail.com>
2026-06-30 11:20:03 -0300

    feat: Criando o card de pergunta e o input do formulário

--- planejaIA/src/components/features/Simulation/Form.tsx
+++ planejaIA/src/components/features/Simulation/Form.tsx
@@ -1,9 +1,21 @@
-import { StepProgress } from "./Progress";
+import { PiggyBank } from 'lucide-react'
+import { FormStep } from './FormStep'
+import { StepProgress } from './Progress'
 
-export function SimulationForm (){
-    return (
-        <>
-        <StepProgress currentStep= {1} totalSteps={6} />
-        </>
-    )
+export function SimulationForm() {
+  return (
+    <>
+      <StepProgress currentStep={1} totalSteps={6} />
+      <FormStep
+        icon={PiggyBank}
+        title="Renda mensal bruta"
+        question="Quanto é depositado na sua conta todo mês (Somente todas as fontes)?"
+        inputProps={{
+            type: 'text',
+            placeholder: 'ex: 5.000,00',
+            prefix: 'R$',
+        }}
+      />
+    </>
+  )
 }
