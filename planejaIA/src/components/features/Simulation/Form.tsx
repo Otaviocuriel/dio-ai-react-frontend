43f88925e8d7276da06d0080d77c21b739b42d6d
Otaviocuriel <otavio.curiel345@gmail.com>
2026-07-01 08:23:48 -0300

    feat: Implementando a navegação entre as etapas do formulário

--- planejaIA/src/components/features/Simulation/Form.tsx
+++ planejaIA/src/components/features/Simulation/Form.tsx
@@ -2,15 +2,41 @@ import { PiggyBank } from 'lucide-react'
 import { FormStep } from './FormStep'
 import { StepProgress } from './Progress'
 import { simulationFormSteps } from '@/data/simulation'
+import { useState } from 'react'
 
 export function SimulationForm() {
-  const currentStep = simulationFormSteps[0]
+  const [inputValue, setInputValue] = useState('')
+  
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+  const totalSteps = simulationFormSteps.length
+  const currentStep = simulationFormSteps[currentStepIndex]
+
+  const handleNextStep = () => {
+    if (currentStepIndex + 1 > totalSteps - 1 ){
+      return
+    }
+    
+    setCurrentStepIndex((prev) => prev + 1)
+  }
+
+  const handlePreviousStep = () =>{
+    if (currentStepIndex === 0) {
+      return
+    }
+    
+    setCurrentStepIndex((prev) => prev - 1 )
+  }
+
   return (
     <>
-      <StepProgress currentStep={1} totalSteps={6} />
+      <StepProgress currentStep={currentStepIndex + 1}
+       totalSteps={totalSteps} />
       <FormStep
         key={currentStep.id}
         {...currentStep}
+        onBack={handlePreviousStep}
+        onNext={handleNextStep}
+        HideBackButton={currentStepIndex === 0}
       />
     </>
   )
